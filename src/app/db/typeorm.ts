5ce980c167eedcdd43b4e38cdbef7cb7a4f9a8cd
seerendo <idarkmer@gmail.com>
2024-06-10 12:13:20 -0500

    Changes server config, .env

--- src/app/db/typeorm.ts
+++ src/app/db/typeorm.ts
@@ -3,15 +3,15 @@ import { UrlORM } from '../../url/repository/orm';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  port: 5432,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  port: Number(process.env.DATABASE_PORT || 5432),
+  host: process.env.DATABASE_HOST || 'localhost',
+  database: process.env.DATABASE_NAME || 'url_shortener',
+  username: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'qwerty',
   entities: [
     UrlORM
   ],
   synchronize: true,
   logging: false,
-  ssl: false,
+  ssl: Boolean(process.env.DATABASE_SSL),
 });
